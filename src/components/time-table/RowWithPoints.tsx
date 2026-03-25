6c5df3e6b8c3f6bf8fb88639760c41b55767db55
m.hollink <m.hollink@dictu.nl>
2026-03-25 13:57:06 +0100

    Highlight correct guesses

--- src/components/time-table/RowWithPoints.tsx
+++ src/components/time-table/RowWithPoints.tsx
@@ -1,14 +1,20 @@
 import {TableCell, TableRow} from "@mui/material";
-import {cell, cellLeft, hoverRow} from "./_styles.ts";
+import {cell, cellLeft, hoverRow, winnerColumnStyle} from "./_styles.ts";
 import type {ScoredWeeklyGuess} from "../../types/data.ts";
 
 export const TableRowWithPoints = ({row}: { row: ScoredWeeklyGuess }) => (
     <TableRow sx={hoverRow}>
         <TableCell/>
         <TableCell sx={cellLeft}>{row.name}</TableCell>
-        <TableCell sx={cell}>{row.guesses.eric} ({row.points!.eric})</TableCell>
-        <TableCell sx={cell}>{row.guesses.niels} ({row.points!.niels})</TableCell>
-        <TableCell sx={cell}>{row.guesses.marcel} ({row.points!.marcel})</TableCell>
+        <TableCell sx={[cell, row.points!.eric === 0 ? winnerColumnStyle : {}]}>
+            {row.guesses.eric} ({row.points!.eric})
+        </TableCell>
+        <TableCell sx={[cell, row.points!.niels === 0 ? winnerColumnStyle : {}]}>
+            {row.guesses.niels} ({row.points!.niels})
+        </TableCell>
+        <TableCell sx={[cell, row.points!.marcel === 0 ? winnerColumnStyle : {}]}>
+            {row.guesses.marcel} ({row.points!.marcel})
+        </TableCell>
         <TableCell sx={cell}>{row.actual}</TableCell>
     </TableRow>
 );
