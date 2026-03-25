732ed3b81b08ca1051b43651195d44760315b9cd
m.hollink <m.hollink@dictu.nl>
2026-03-25 14:05:26 +0100

    make the table scrollable

--- src/components/time-table/TimeTable.tsx
+++ src/components/time-table/TimeTable.tsx
@@ -1,7 +1,7 @@
 import type {FunctionComponent} from "react";
 import type {WeeklyGuess} from "../../types/data.ts"
 
-import {Table, TableBody, TableCell, TableHead, TableRow,} from "@mui/material";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import {GlassCard} from "../GlassCard.tsx";
 import {RowGroup} from "./RowGroup.tsx";
 
@@ -14,27 +14,34 @@ export type TimeTableProps = {
 export const TimeTable: FunctionComponent<TimeTableProps> = ({data}) => {
     return (
         <GlassCard title={"Arrival times & Guesses"}>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={{...headerStyle, textAlign: "left"}}>Date</TableCell>
-                        <TableCell sx={{...headerStyle, textAlign: "left"}}>Team member</TableCell>
-                        <TableCell sx={headerStyle}>Eric</TableCell>
-                        <TableCell sx={headerStyle}>Niels</TableCell>
-                        <TableCell sx={headerStyle}>Marcel</TableCell>
-                        <TableCell sx={headerStyle}>Actual</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {Object
-                        .entries(data)
-                        .sort(([weekA], [weekB]) => new Date(weekB).getTime() - new Date(weekA).getTime())
-                        .slice(0, 4)
-                        .map(([week, rows]) => (
-                            <RowGroup key={week} week={week} rows={rows}/>
-                        ))}
-                </TableBody>
-            </Table>
+            <TableContainer
+                sx={{
+                    width: '100%',
+                    overflowX: {xs: 'scroll', md: 'auto'},
+                }}
+            >
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{...headerStyle, textAlign: "left"}}>Date</TableCell>
+                            <TableCell sx={{...headerStyle, textAlign: "left"}}>Team member</TableCell>
+                            <TableCell sx={headerStyle}>Eric</TableCell>
+                            <TableCell sx={headerStyle}>Niels</TableCell>
+                            <TableCell sx={headerStyle}>Marcel</TableCell>
+                            <TableCell sx={headerStyle}>Actual</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {Object
+                            .entries(data)
+                            .sort(([weekA], [weekB]) => new Date(weekB).getTime() - new Date(weekA).getTime())
+                            .slice(0, 4)
+                            .map(([week, rows]) => (
+                                <RowGroup key={week} week={week} rows={rows}/>
+                            ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </GlassCard>
     );
 }
